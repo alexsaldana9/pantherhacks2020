13a021b8cfdcbe0b31d20b7271e1ef5df675309f
alexsaldana9 <alexsaldana9@gmail.com>
2020-07-18 19:31:06 -0400

    styles

--- covid19/routes/instructions.js
+++ covid19/routes/instructions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('instructions', { title: 'Instructions for Employees' });
+  res.render('instructions', { title: 'Empoyee Training' });
 });
 
 module.exports = router;
